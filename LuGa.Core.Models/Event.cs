907356a57cc92cf00a6bc277df136502ca425182
Gavan Lamb <gavan.jon.lamb@gmail.com>
2018-02-25 23:38:20 +1100

    Initial commit

---
+++ LuGa.Core.Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace LuGa.Core.Device.Models
+{
+    public class Event
+    {
+        public string DeviceId { get; set; }
+        
+        public string Action{ get; set; }
+        
+        public string Zone{ get; set; }
+    }
+}
---
+++ LuGa.Core.Models/Reading.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LuGa.Core.Device.Models
+{
+    public class Reading
+    {
+        public string DeviceId { get; set; }
+
+        public string ReadingType { get; set; }
+
+        public string Value { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
