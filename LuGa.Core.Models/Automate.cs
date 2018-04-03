d43b340ec6efdd90986ca2fbe385c1c929cab127
Gavan Lamb <gavan.jon.lamb@gmail.com>
2018-04-03 10:30:48 +0000

    Merged PR 91: Merged PR 90: add automate model
    Merged PR 90: add automate model
    
    add automate model
    
    Related work items: #200
    
    Related work items: #200

---
+++ LuGa.Core.Models/Automate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LuGa.Core.Models
+{
+    public class Automate
+    {
+        public int Id { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public string AverageReading { get; set; }
+    }
+}
